2fa9cc0a6b87ff63171e34c747aecaba43e5ff42
Agustin Gallegos <agallego@microsoft.com>
2023-01-11 00:53:22 -0300

    update functions names

--- ExoGraphGUI/functions/Remove-ExoGraphGUIAADAppData.ps1
+++ ExoGraphGUI/functions/Remove-ExoGraphGUIAADAppData.ps1
@@ -1,10 +1,10 @@
-﻿function Remove-EWsGuiAADAppData {
+﻿function Remove-ExoGraphGUIAADAppData {
     <#
     .SYNOPSIS
-    Function to remove ClientID, TenantID and ClientSecret to the EWSGui powershell module.
+    Function to remove ClientID, TenantID and ClientSecret to the ExoGraphGUI powershell module.
     
     .DESCRIPTION
-    Function to remove ClientID, TenantID and ClientSecret to the EWSGui powershell module.
+    Function to remove ClientID, TenantID and ClientSecret to the ExoGraphGUI powershell module.
     
     .PARAMETER Confirm
     If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
@@ -13,9 +13,9 @@
     If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
 
     .EXAMPLE
-    PS C:\> Remove-EWsGuiAADAppData
+    PS C:\> Remove-ExoGraphGUIAADAppData
 
-    The script will Remove these values in the EWSGui module to be used automatically.
+    The script will Remove these values in the ExoGraphGUI module to be used automatically.
     #>
     [CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = 'Low')]
     param (
@@ -27,11 +27,11 @@
     }
     
     process {
-        Write-PSFMessage -Level Important -Message "Removing ClientID, TenantID and ClientSecret strings from EWSGui Module."
-        Unregister-PSFConfig -Module EWSGui
-        remove-PSFConfig -Module ewsgui -Name clientID -Confirm:$false
-        remove-PSFConfig -Module ewsgui -Name tenantID -Confirm:$false
-        remove-PSFConfig -Module ewsgui -Name ClientSecret -Confirm:$false
+        Write-PSFMessage -Level Important -Message "Removing ClientID, TenantID and ClientSecret strings from ExoGraphGUI Module."
+        Unregister-PSFConfig -Module ExoGraphGUI
+        remove-PSFConfig -Module ExoGraphGUI -Name clientID -Confirm:$false
+        remove-PSFConfig -Module ExoGraphGUI -Name tenantID -Confirm:$false
+        remove-PSFConfig -Module ExoGraphGUI -Name ClientSecret -Confirm:$false
     }
     
     end {
